b49cc4022398864b89a366fe65e67fff9bf68761
Smirnov Ivan <killipsmirnov@yandex.ru>
2017-04-18 00:54:11 +0300

    C2S-2 - Добавлени в проект squeryl и основного Service

---
+++ app/models/DatabaseSchema.scala
@@ -0,0 +1,15 @@
+package models
+
+import org.squeryl.Schema
+
+/**
+  *
+  * Created with IntelliJ IDEA.
+  * User: Смирнов Иван (killips)
+  * Date: 18.04.2017
+  *
+  */
+
+object DatabaseSchema extends Schema {
+
+}
